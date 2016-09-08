6c9eb9d01bdbf3f32871a2d864a9cd58106f9c19
Jimmy Wärting <jimmy@warting.se>
2016-09-08 18:11:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Screw-FileReader
-For those lazzy bastard who don't like the FileReader
+For those lazzy bastard who don't like the FileReader :)
+
+A promisified Blob reader was [proposed][proposed].
+But not as a external API like FileReader... Nooo - this is prototyped to blob directly!
+
+This makes it easier to read stuff from Blob's and File's
+
+Here is an example
+
+```javascript
+arrayBuffer = await blob.arrayBuffer() // awaits a promise
+text = await blob.text() // awaits a promise
+json = await blob.json() // awaits a promise
+stream = blob.stream() // returns a web ReadableStream
+```
+
+if streams are enhanced with [web-streams-polyfill][polyfill] then you get all
+the benefits of pipes as well :v:
+
+  [polyfill]: https://github.com/creatorrr/web-streams-polyfill
+  [proposed]: https://github.com/w3c/FileAPI/issues/40
---
+++ index.js
@@ -0,0 +1,121 @@
+;(function() {
+
+	if(typeof Blob === 'undefined')
+		return console.warn('Screw-FileReader is only meant to work in those' +
+	'engine who already has some basic support for blob')
+
+	var blob = Blob.prototype
+	var fullStreamSupport = false
+	var basicStreamSupport = false
+	var fetchTransform = false
+
+	try {
+		new ReadableStream({})
+		basicStreamSupport = true
+	} catch (e) {}
+
+	try {
+		new ReadableStream({type: 'bytes'})
+		fullStreamSupport = true
+	} catch (e) {}
+
+	try {
+		(new Response(new Blob)).getReader()
+		fetchTransform = true
+	} catch (e) {}
+
+	if(!blob.arrayBuffer) {
+		blob.arrayBuffer = function arrayBuffer() {
+			var fr = new FileReader()
+			fr.readAsArrayBuffer(this)
+			return new Promise(function(resolve, reject) {
+				fr.onload = function(evt) { resolve(evt.target.result) }
+				fr.onerror = function(evt) { reject(evt.target.error) }
+			})
+		}
+	}
+
+	if(!blob.text) {
+		blob.text = function text() {
+			var fr = new FileReader()
+			fr.readAsText(this)
+			return new Promise(function(resolve, reject) {
+				fr.onload = function(evt) { resolve(evt.target.result) }
+				fr.onerror = function(evt) { reject(evt.target.error) }
+			})
+		}
+	}
+
+	if(!blob.json) {
+		blob.json = function json() {
+			return this.text().then(JSON.parse)
+		}
+	}
+
+	if(!blob.stream) {
+		console.log(fullStreamSupport)
+		blob.stream =
+
+		fullStreamSupport ? function stream() {
+			var position = 0
+			var blob = this
+
+			return new ReadableStream({
+				type: 'bytes',
+				autoAllocateChunkSize: 524288,
+
+				pull: function(controller) {
+					var v = controller.byobRequest.view
+					var chunk = blob.slice(position, position + v.byteLength)
+					return chunk.arrayBuffer()
+					.then(function(buffer) {
+						let uint8array = new Uint8Array(buffer)
+						let bytesRead = uint8array.byteLength
+
+						position += bytesRead
+						v.set(uint8array)
+							controller.byobRequest.respond(bytesRead)
+
+						if(position >= blob.size)
+							controller.close()
+
+						resolve()
+					})
+				}
+			})
+		}:
+
+		// basic stream support
+		basicStreamSupport ? function stream(blob){
+			var position = 0
+			var blob = this
+
+			return new ReadableStream({
+				pull: function(controller) {
+					if(currentChunk == chunks)
+						return controller.close()
+
+					var chunk = blob.slice(position, position + 524288)
+
+					return chunk.arrayBuffer().then(function(buffer) {
+						let uint8array = new Uint8Array(buffer)
+						controller.enqueue(uint8array)
+
+						if(position == blob.size)
+							controller.close()
+					})
+				}
+			})
+		}:
+
+		// fetchTransform
+		fetchTransform ? function stream() {
+			return (new Response(this)).body
+		}:
+
+		function stream() {
+			throw new Error('Include https://github.com/creatorrr/web-streams-polyfill')
+		}
+	}
+
+}());
